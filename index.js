40d60777b9feb20318358f71308a96d6631cdb0d
shahadat hossen <hossantopu@gmail.com>
2024-08-10 13:58:17 +0600

    set environment variable

--- index.js
+++ index.js
@@ -11,8 +11,31 @@ const Comment = require('./models/Comment')
 const ContactList = require('./models/ContactList')
 const Data = require('./models/Data')
 
+require('dotenv').config()
+
 app.use(cors())
 app.use(routes)
+
+app.get('/comments',async(req,res,next)=>{
+    try{
+        const comments=await Comment.find()
+        res.status(200).json(comments)
+    }catch{
+        next(error)
+    }
+})
+
+app.delete('/comment/:id',async(req,res,next)=>{
+    const id=req.params.id
+    try{
+        const comment=await Comment.deleteOne({
+            _id:id
+        })
+        res.status(200).json(comment)
+    }catch{
+        next(error)
+    }
+})
 
 app.use((err,req,res,next)=>{
     const message=err.message?err.message:'Server Error Occurred'
@@ -21,17 +44,14 @@ app.use((err,req,res,next)=>{
     res.status(status).json(message)
 })
 
-connectDB('mongodb+srv://shahadat:shahadat1234@portfolio.d8fxm.mongodb.net/portfolio')
+connectDB(process.env.MONGO_URL)
 .then(()=>{
     console.log('Database Connected')
 
-    app.listen(port,()=>{
+    app.listen(process.env.PORT,()=>{
         console.log('server is running')
     })
 })
 .catch(()=>{
     throw error()
 })
-
-
-// shahadat1234
